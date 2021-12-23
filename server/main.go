6b0b375d2994bebc7c6383522945cadd6d4cb9c9
anthem-landing18 <imokenpi2011@gmail.com>
2021-12-24 02:47:19 +0900

    サーバの起動処理を追加

--- server/main.go
+++ server/main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"log"
-
 	"github.com/imokenpi2011/fotune-slipper/server/app/controllers"
 )
 
 func main() {
-	log.Println("start main method.")
 	controllers.StartMainServer()
-
-	// fotune, err := services.GetFotuneResult()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Println(fotune)
-	log.Println("End main method.")
 }
